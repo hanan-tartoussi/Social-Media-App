aabc9df364a20ff3e892cd79385d80b6f33afad8
Wafaa Al Jarrah <wafaajarrah99@gmail.com>
2023-01-29 00:17:50 +0200

    Update HomeScreen.js

--- Screen/HomeScreen.js
+++ Screen/HomeScreen.js
@@ -8,10 +8,10 @@ import Header from '../Components/Header';
 
 state = {
     UsersPosts: [
-        { id: 1, userImg: require('../android/app/src/main/assets/images/userProf.png'), name: 'Wafaa Al Jarrah', date: '4 mins', postText: 'Hello!! This is my post', postImg: require('../android/app/src/main/assets/images/postImg.png') },
-        { id: 2, userImg: require('../android/app/src/main/assets/images/user2.png'), name: 'Ahmad', date: 'Jan 1,2023', postText: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...', postImg: '' },
-        { id: 3, userImg: require('../android/app/src/main/assets/images/user3.png'), name: 'Kareem', date: 'Dec 10,2022', postText: 'There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...', postImg: require('../android/app/src/main/assets/images/post2.png') },
-        { id: 4, userImg: require('../android/app/src/main/assets/images/user4.png'), name: 'Hanan Tartoussi', date: 'Oct 10,2021', postText: 'Hello!!!', postImg: '' }
+        { id: 1, userImg: require('../Images/userProf.png'), name: 'Wafaa Al Jarrah', date: '4 mins', postText: 'Hello!! This is my post', postImg: require('../android/app/src/main/assets/images/postImg.png') },
+        { id: 2, userImg: require('../Images/user2.png'), name: 'Ahmad', date: 'Jan 1,2023', postText: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...', postImg: '' },
+        { id: 3, userImg: require('../Images/user3.png'), name: 'Kareem', date: 'Dec 10,2022', postText: 'There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...', postImg: require('../android/app/src/main/assets/images/post2.png') },
+        { id: 4, userImg: require('../Images/user4.png'), name: 'Hanan Tartoussi', date: 'Oct 10,2021', postText: 'Hello!!!', postImg: '' }
     ],
     
 }
