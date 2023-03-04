a26d0853b4a93a483a9e76bfb001f603beab2c82
Shahinaz-Wehbi <shahinazwehbi9sh.w@gmail.com>
2023-03-04 20:50:22 +0200

    old version of App.js

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import Providers from './Navigation';
 import AppStack from './Navigation/AppStack';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
   return (
     <>
-
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <Providers />
-      </GestureHandlerRootView>
+      <Providers />
       {/* <NavigationContainer>
         <AppStack />
       </NavigationContainer> */}
